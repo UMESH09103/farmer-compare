962d4aa0bd892210247e03d753dd03af967e0c23
umesh <umeshrj629@gmail.com>
2025-03-21 01:31:21 +0530

    Initial commit for farmer-compare repository

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // frontend/src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/home.jsx';
+import Home from './components/Home.jsx';
 import ShopperDashboard from './components/ShopperDashboard.jsx';
 import ShopProducts from './components/ShopProducts.jsx';
 import Login from './components/Login.jsx';
