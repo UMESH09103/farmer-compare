56f13936acd5102bca4cae1cebf6732048597814
umesh <umeshrj629@gmail.com>
2025-03-21 02:03:52 +0530

    Initial commit for farmer-compare repository

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
